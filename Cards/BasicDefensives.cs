c5059d8cb6a3c9b9da7404d0dd1edf21711ba08a
UnicornArin <freezie0@gmail.com>
2023-10-30 21:38:27 -0500

    Add files via upload
    Basic Defensives Card

---
+++ Cards/BasicDefensives.cs
@@ -0,0 +1,75 @@
+﻿﻿using JohannaTheTrucker.Actions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JohannaTheTrucker.Cards
+{
+    [CardMeta(rarity = Rarity.common, upgradesTo = new Upgrade[] { Upgrade.A, Upgrade.B })]
+    public class BasicDefensives : Card
+    {
+        public override List<CardAction> GetActions(State s, Combat c)
+        {
+            var result = new List<CardAction>();
+            switch (upgrade)
+            {
+               
+                case Upgrade.None:
+                    result.Add(new AStatus() { 
+						status=Status.evade,
+						statusAmount=1,
+						targetPlayer=true
+                    });
+                    result.Add(new AStatus()
+                    {
+                        status=Status.shield, 
+                        statusAmount=1,
+                        targetPlayer = true
+                    });
+                    break;
+                case Upgrade.A:
+					result.Add(new AStatus()
+                    {
+                        status = Status.shield,
+                        statusAmount = 1,
+                        targetPlayer = true
+                    });
+                    result.Add(new AStatus()
+                    {
+                        status = Status.evade,
+                        statusAmount = 2,
+                        targetPlayer = true
+                    });
+                    
+                    break;
+                case Upgrade.B:
+                    result.Add(new AStatus()
+                    {
+                        status = Status.evade,
+                        statusAmount = 1,
+                        targetPlayer = true
+                    });
+                    result.Add(new AStatus()
+                    {
+                        status = Status.shield,
+                        statusAmount = 2,
+                        targetPlayer = true
+                    });
+                    break;
+            }
+            return result;
+        }
+
+        public override CardData GetData(State state)
+        {
+            return new CardData
+            {
+                cost = 2,
+            };
+        }
+
+        public override string Name() => "Basic Defensives";
+    }
+}
